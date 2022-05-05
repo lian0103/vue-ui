1b27b77b7c15b61470080340bb3104f26b6230fc
Jason <gt07891@greattree.com.tw>
2022-05-05 11:40:26 +0800

    demo page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
@@ -7,60 +7,69 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.js'),
-      name: 'gt-UI',
-      fileName: (format) => `gt-UI.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
-        globals: {
-          vue: 'Vue',
+export default ({ mode }) => {
+  let config = {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/components/index.js'),
+        name: 'gt-UI',
+        fileName: (format) => `gt-UI.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // Add external deps here
+          globals: {
+            vue: 'Vue',
+          },
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/assets/elementPlus.scss" as *;`,
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
     },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
-          // directives: true,
-          // version: "2.1.5",
-        }),
-      ],
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass',
-          // directives: true,
-          // version: "2.1.5",
-        }),
-      ],
-    }),
-    svgLoader(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-});
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/elementPlus.scss" as *;`,
+        },
+      },
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass',
+            // directives: true,
+            // version: "2.1.5",
+          }),
+        ],
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass',
+            // directives: true,
+            // version: "2.1.5",
+          }),
+        ],
+      }),
+      svgLoader(),
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+    },
+  };
+
+  if (mode === 'demo') {
+    delete config['build'];
+    // console.log(config);
+  }
+
+  return defineConfig(config);
+};
