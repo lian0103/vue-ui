07c756eed3c1ca07cb139c78c749ab288652de5d
Jason <k1319900103@gmail.com>
2022-05-25 15:17:01 +0800

    fix GInput css

--- src/components/GInput.vue
+++ src/components/GInput.vue
@@ -155,7 +155,7 @@ const handleClear = () => {
     width: 100%;
     max-width: 240px;
     height: 36px;
-    padding: 10px 0px 10px 20px;
+    padding: 10px 22px 10px 20px;
     font-size: 16px;
     color: #303236;
     background: #f0f0f0;
