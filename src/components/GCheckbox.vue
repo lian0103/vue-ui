bc28006a2c8576ba82bc29974abe139d78c9b856
Jason <k1319900103@gmail.com>
2022-06-09 16:43:53 +0800

    回饋修正 ->  checkbox 0608

--- src/components/GCheckbox.vue
+++ src/components/GCheckbox.vue
@@ -10,29 +10,45 @@ export default {
 import { computed, ref } from 'vue';
 import GIcons from './GIcons.vue';
 
-const { disabled, modelValue, label, value, parentValue, handleChildClick } =
-  defineProps({
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    modelValue: {
-      type: Boolean,
-      default: false,
-    },
-    label: {
-      default: null,
-    },
-    value: {
-      default: null,
-    },
-    parentValue: {
-      default: null,
-    },
-    handleChildClick: {
-      default: null,
-    },
-  });
+const CheckboxEnum = {
+  GREEN: 'box-green',
+  WHITE: 'box-white',
+};
+
+const {
+  disabled,
+  modelValue,
+  label,
+  value,
+  parentValue,
+  handleChildClick,
+  type,
+} = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  label: {
+    default: null,
+  },
+  value: {
+    default: null,
+  },
+  parentValue: {
+    default: null,
+  },
+  handleChildClick: {
+    default: null,
+  },
+  type: {
+    type: String,
+    default: 'GREEN',
+  },
+});
 
 const isChecked = parentValue
   ? computed(() => parentValue?.value?.includes(value))
@@ -41,7 +57,12 @@ const isChecked = parentValue
 const emit = defineEmits(['update:modelValue']);
 
 const classComputed = computed(() => {
-  let arr = ['checkmark'];
+  let arr = [
+    'checkmark',
+    CheckboxEnum[type.toUpperCase()]
+      ? CheckboxEnum[type.toUpperCase()]
+      : CheckboxEnum['GREEN'],
+  ];
   if (isChecked.value) arr.push('checked');
   if (disabled) arr.push('disabled');
   return arr;
@@ -64,7 +85,7 @@ const onClick = () => {
   <div class="gt-checkbox" @click.prevent="onClick">
     <input type="checkbox" :checked="isChecked" />
     <div :class="classComputed">
-      <g-icons v-show="isChecked" class="icon" name="check" size='sm' />
+      <g-icons v-show="isChecked" class="icon" name="check" size="sm" />
     </div>
     <span
       class="label select-none"
@@ -76,6 +97,16 @@ const onClick = () => {
 </template>
 
 <style lang="scss">
+.box-green {
+  --bg-color: #eef5eb;
+  --border-color: #32a03d;
+}
+
+.box-white {
+  --bg-color: #fff;
+  --border-color: #aaa;
+}
+
 .gt-checkbox {
   min-width: 17px;
   height: 17px;
@@ -83,14 +114,16 @@ const onClick = () => {
   input {
     @apply absolute w-0 h-0 opacity-0 cursor-pointer;
   }
+
   .checkmark {
     width: 17px;
     height: 17px;
     @apply absolute left-0 rounded-sm;
     top: 2px;
     border-width: 1.5px;
-    @apply border-solid border-main;
-    @apply bg-color1;
+    @apply border-solid;
+    border-color: var(--border-color);
+    background-color: var(--bg-color);
     @apply transition-all duration-300;
   }
 
