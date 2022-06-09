667dda3951558e46c3868099e4d20b9e57ac5d49
Jason <k1319900103@gmail.com>
2022-06-09 12:16:32 +0800

    回饋修正 ->  letter spacing 0608

--- src/components/GTitle.vue
+++ src/components/GTitle.vue
@@ -18,21 +18,21 @@ const { level } = defineProps({
 
 .gt-level0 {
   @apply text-base leading-6;
-  word-spacing: 0.8px;
+  letter-spacing: 0.8px;
 }
 
 .gt-level1 {
   @apply font-bold text-2xl leading-8;
-  word-spacing: 1.2px;
+  letter-spacing: 1.2px;
 }
 
 .gt-level2 {
   @apply font-medium text-xl leading-7;
-  word-spacing: 1px;
+  letter-spacing: 1px;
 }
 
 .gt-level3 {
   @apply font-light text-lg leading-6;
-  word-spacing: 0.9px;
+  letter-spacing: 0.9px;
 }
 </style>
