e5bd3ada7aff0a54394f319b34a57f80a2d3b1e6
Jason <gt07891@greattree.com.tw>
2022-06-29 09:19:07 +0800

    deploy

---
+++ assets/GTitle.de7cdd06.js
@@ -0,0 +1,1 @@
+import{_ as l,o as s,c as r,r as a,s as o}from"./index.af9aaa30.js";const _={props:{level:{type:Number,default:0}},setup(e){return(t,n)=>(s(),r("div",{class:o(["gt-title","gt-level"+e.level])},[a(t.$slots,"default")],2))}};var i=l(_,[["__file","D:/frontEnd/gt-UI/src/components/GTitle.vue"]]);export{i as G};
