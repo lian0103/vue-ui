b4387e29eaa2e5b3d0a8a86140da7689b114eb0c
lian0103 <k1319900103@gmail.com>
2022-07-22 20:48:08 +0800

    fix: Radiobox 改用svg

--- src/components/GRadiobox.vue
+++ src/components/GRadiobox.vue
@@ -53,13 +53,64 @@ const onClick = () => {
 </script>
 
 <template>
-  <div class="gt-radiobox" @click.prevent="onClick">
-    <div
-      class="checkmark"
-      :class="(isChecked ? 'checked' : '') + ' ' + (disabled ? 'disabled-outline' : '')"
+  <div
+    class="gt-radiobox"
+    :class="disabled ? 'disabled' : ''"
+    @click.prevent="onClick"
+  >
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <div class="circle" :class="isChecked?'show':''"></div>
-    </div>
+      <rect
+        x="0.5"
+        y="0.5"
+        width="19"
+        height="19"
+        rx="9.5"
+        :fill="
+          disabled
+            ? isChecked
+              ? '#AAAAAA'
+              : ''
+            : isChecked
+            ? '#32A03D'
+            : '#EEF5EB'
+        "
+      />
+      <circle
+        :class="isChecked ? 'circle show' : 'circle'"
+        cx="10"
+        cy="10"
+        r="5.25"
+        :fill="
+          disabled
+            ? isChecked
+              ? '#AAAAAA'
+              : ''
+            : isChecked
+            ? '#32A03D'
+            : '#EEF5EB'
+        "
+      />
+      <path
+        v-show="isChecked"
+        d="M18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10ZM4.75173 10C4.75173 12.8985 7.10146 15.2483 10 15.2483C12.8985 15.2483 15.2483 12.8985 15.2483 10C15.2483 7.10146 12.8985 4.75173 10 4.75173C7.10146 4.75173 4.75173 7.10146 4.75173 10Z"
+        fill="white"
+      />
+      <rect
+        x="0.5"
+        y="0.5"
+        width="19"
+        height="19"
+        rx="9.5"
+        :stroke="disabled ? '#AAA' : '#32A03D'"
+      />
+    </svg>
+
     <span
       class="label"
       v-if="label"
@@ -77,42 +128,23 @@ const onClick = () => {
   height: 20px;
   min-width: 20px;
   @apply relative cursor-pointer flex justify-start;
+  
+  &.disabled {
+    @apply cursor-not-allowed;
+  }
 
-  .checkmark {
-    width: 16px;
-    height: 16px;
-    @apply relative;
-    left: 0px;
-    top: 0px;
+  svg {
     margin-right: 10px;
-    @apply rounded-full bg-color1;
-    @apply outline-2 outline-main outline;
-    @apply transition-all duration-300;
-    @apply flex justify-center items-center;
   }
 
-  .checked {
-    .circle {
-      width: 10px;
-      height: 10px;
-      background-image: url('../assets/icons/dot-green.svg');
-      background-repeat: no-repeat;
-      background-size: cover;
-      opacity: 0;
-      &.show{
-        opacity: 1;
-      }
+  .circle {
+    display: none;
+    @apply transition-all duration-300;
+    &.show {
+      display: block;
     }
   }
-  .disabled-outline {
-    @apply cursor-not-allowed bg-white;
-    @apply outline-2 outline-gray0 outline;
-    .circle {
-      background-image: url('../assets/icons/dot-gray.svg');
-      background-repeat: no-repeat;
-      background-size: cover;
-    }
-  }
+
   .label {
     @apply select-none;
     &.label-disabled {
