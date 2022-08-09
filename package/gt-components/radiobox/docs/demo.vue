054e5304259f0b45bc09a19cea1be11bd674ce06
Jason <k1319900103@gmail.com>
2022-08-09 15:20:50 +0800

    add radio-group

--- package/gt-components/radiobox/docs/demo.vue
+++ package/gt-components/radiobox/docs/demo.vue
@@ -5,6 +5,7 @@ const inputs = reactive({
   radio1: false,
   radio2: true,
   radio3: false,
+  radio4: 2,
 });
 </script>
 
@@ -16,5 +17,13 @@ const inputs = reactive({
       <g-radiobox class="mr-4" v-model="inputs.radio2" disabled label="C" />
       <g-radiobox class="mr-4" v-model="inputs.radio3" disabled label="D" />
     </div>
+
+    <p class="py-2">inputs.radio4:{{ inputs.radio4 }}</p>
+    <g-radiobox-group class="mb-3 flex w-full" v-model="inputs.radio4">
+      <g-radiobox class="mr-4" :value="2" label="選項2" />
+      <g-radiobox class="mr-4" :value="4" label="選項4" />
+      <g-radiobox class="mr-4" :value="6" label="選項6" />
+      <g-radiobox class="mr-4" :value="8" label="選項8" disabled />
+    </g-radiobox-group>
   </div>
 </template>
