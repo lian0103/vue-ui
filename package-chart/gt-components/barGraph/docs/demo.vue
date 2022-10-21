36819383d514d04d91fc0504e2eda9a509043d37
Jason <k1319900103@gmail.com>
2022-10-21 17:33:01 +0800

    update: docs

---
+++ package-chart/gt-components/barGraph/docs/demo.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { reactive, ref } from 'vue';
+const title = reactive({
+  text: 'text',
+  subtext: 'subtext',
+});
+const direction = ref('vertical');
+const keys = ['mon', 'tue', 'wed', 'thu', 'fri'];
+const values = [
+  {
+    data: [100, 200, 230, 250, 50],
+    name: '第一家廠商',
+  },
+  {
+    data: [100, 270, 20, 500, 600],
+    name: '第二家廠商',
+  },
+  {
+    data: [100, 200, 230, 250, 50],
+    name: '第三家廠商',
+  },
+  {
+    data: [100, 270, 20, 500, 600],
+    name: '第四廠商',
+  },
+];
+const changeBtn = () => {
+  if (direction.value === 'horizen') {
+    direction.value = 'vertical';
+  } else {
+    direction.value = 'horizen';
+  }
+};
+</script>
+<template>
+  <g-button @click="changeBtn">更換方向</g-button>
+  <g-divider />
+  <g-bar-graph
+    v-model="values"
+    :title="title"
+    :showTip="true"
+    :showLegend="true"
+    :showBackground="true"
+    :direction="direction"
+    :keys="keys"
+  ></g-bar-graph>
+</template>
+<style scoped>
+.toolList {
+  margin-bottom: 5px;
+}
+</style>
---
+++ package-chart/gt-components/barGraph/docs/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  show: ['prop'],
+  prop: `
+## 屬性
+| 屬性 | 說明 | 類型 | 可選值 | 默認值 |
+| - | - | - |- | -  |
+| v-model | 渲染的資料，必填 | Array | | |
+| key | 標題的值 | Array | | |
+| title | 標題的字 | Object | | |
+| showTip | 顯示提示訊息 | Boolean | | true |
+| showLegend | 顯示圖例 | Boolean | | true |
+| showBackground | 顯示item背景 | Boolean | | true |
+| direction | 長條圖方向 | String | horizen/vertical | horizen |
+| height | 高度 | String |  | 200px |
+
+`,
+  method: `
+## 方法
+| 方法 | 說明 | 參數 |
+| - | - | - |
+| | | |
+
+`,
+  event: `
+## 事件
+| 事件 | 說明 | 參數 |
+| - | - | - |
+| | | |
+
+`,
+  slot: `
+## 插槽
+| 插槽 | 說明 | 
+| - | - | 
+| | | 
+
+`,
+};
