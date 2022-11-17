465d630fb2b6f9c60201a451f7f50a01a6ebe4f4
ziv.huang <ziv.huang@greattree.com.tw>
2022-11-17 11:43:55 +0800

    fix: layout menu table tabs

--- package/gt-components/layout/src/layout.vue
+++ package/gt-components/layout/src/layout.vue
@@ -77,10 +77,9 @@ watch(
 const handleMenuTabCallback = (target = {}) => {
   let { path, query } = target;
   if (path && Router) {
-
     Router.push({
-      path: path, 
-      query: query
+      path: path,
+      query: query,
     });
   } else {
     console.log("target is empty");
@@ -130,21 +129,21 @@ export default {
       <div class="gt-sidebar" :class="onlyOneLevel ? 'onlyOneLevel' : ''">
         <div class="gt-menu-box">
           <slot name="sidebar" />
-        </div>
-
-        <div class="gt-bottomLogo">
-          <img
-            style="width: 28.58px; height: 28.09px"
-            src="../../../assets/images/greattree-icon.svg"
-            alt=""
-          />
-          <img
-            style="width: 80px; height: 18.41px"
-            v-if="!isCollapsed"
-            src="../../../assets/images/greattree-text.svg"
-            alt=""
-          />
-          <!-- <img src="../../../assets/svg/gt-icon-with-text.svg"/> -->
+          <div class="gt-bottomLogo">
+            <div>
+              <img
+                style="width: 28.58px; height: 28.09px"
+                src="../../../assets/images/greattree-icon.svg"
+                alt=""
+              />
+              <img
+                style="width: 80px; height: 18.41px"
+                v-if="!isCollapsed"
+                src="../../../assets/images/greattree-text.svg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
 
@@ -160,7 +159,7 @@ export default {
           name="layoutTab"
           v-if="onlyOneLevel && menuTabs && menuTabs.length > 0"
           class="gt-route-tabs"
-          :tabs="menuTabs" 
+          :tabs="menuTabs"
           :clickCallback="handleMenuTabCallback"
           :currentTab="currentTab"
           :layoutMode="true"
@@ -171,7 +170,7 @@ export default {
           class="gt-content"
           :style="{
             height:
-            onlyOneLevel && menuTabs && menuTabs.length > 0
+              onlyOneLevel && menuTabs && menuTabs.length > 0
                 ? 'calc(100vh - 125px)'
                 : 'calc(100vh - 80px)',
           }"
