385eee63a03bd480fdfb4dbeedd19dc3fa501878
Jason <k1319900103@gmail.com>
2022-11-24 11:14:50 +0800

    update: doc

--- src/gtDoc.js
+++ src/gtDoc.js
@@ -524,13 +524,28 @@ const options = [
     { label: 'Bbbb', value: 2 },
     { label: 'Ggggwwwwwwwsssswww', value: 3 },
 ];
+
+const handleClick = (e) => {
+    console.log('click', e);
+};
 </script>
 
 <template>
     <div class="w-full mx-auto mb-6 relative">
+        <g-title :level="2" class="mb-3">with slot dropdownItem</g-title>
+        <g-dropdown class="mr-4" v-model="inputs.dropdown0">
+            <g-dropdown-item
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+                @itemClick="handleClick"
+            />
+        </g-dropdown>
+        <g-divider />
+
         <g-title :level="2" class="mb-3">下拉選項點擊模式</g-title>
         <g-dropdown class="mr-4" v-model="inputs.dropdown0" :options="options" />
-
         <g-divider />
 
         <g-title :level="2" class="mb-3">下拉選項停留模式</g-title>
@@ -541,12 +556,11 @@ const options = [
         <div class="w180">
             <g-dropdown v-model="inputs.dropdown1" :options="options" icon hover />
         </div>
-
     </div>
 </template>
 
 <style lang="scss">
-.w180{
+.w180 {
     width: 180px;
 }
 </style>
